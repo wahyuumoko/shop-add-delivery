d2fc09bc81e482fe51488187021b2569db24fcf7
wahyuprihatmoko788 <wahyuprihatmoko788@gmail.com>
2024-07-25 13:53:36 +0000

    tambahan 1

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -13,25 +13,45 @@ class CartPage extends StatelessWidget {
         // cart
         final userCart = shop.cart;
 
-        //scaffold ui
+        // scaffold UI
         return Scaffold(
           appBar: AppBar(
             title: const Text("Cart"),
             backgroundColor: Colors.transparent,
             foregroundColor: Theme.of(context).colorScheme.inversePrimary,
             actions: [
-              //clear button cart
+              // clear cart button
               IconButton(
+                icon: const Icon(Icons.delete),
                 onPressed: () {
-                  shop.clearCart();
-                }, 
-                icon: const Icon(Icons.delete)
-                ),
-              //checkout button cart
+                  showDialog(
+                    context: context, 
+                    builder: (context) => AlertDialog(
+                      title: const Text("Are you sure you want to clear?"),
+                      actions: [
+                        // cancel button
+                        TextButton(
+                          onPressed: () => Navigator.pop(context), 
+                          child: const Text("Cancel"),
+                        ),
+                        // yes button
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            shop.clearCart();
+                          }, 
+                          child: const Text("Yes"),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+              // checkout button
               IconButton(
                 onPressed: () {}, 
-                icon: const Icon(Icons.check)
-                )
+                icon: const Icon(Icons.check),
+              ),
             ],
           ),
           body: Column(
