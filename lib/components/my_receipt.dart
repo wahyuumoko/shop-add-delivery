88869b1b7a6588e3ee38ba8da99edcf99b427823
wahyuprihatmoko788 <wahyuprihatmoko788@gmail.com>
2024-07-29 03:35:41 +0000

    receipt+

--- lib/components/my_receipt.dart
+++ lib/components/my_receipt.dart
@@ -5,21 +5,25 @@ class MyReceipt extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
+    return Padding(padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25, top: 25),
+    child: Center (
     child: Column(
-      children: [
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
         const Text("Thank you for your order!"),
+        const SizedBox(height: 25),
         Container(
           decoration: BoxDecoration(
            border: 
            Border.all(color:Theme.of(context).colorScheme.secondary),
            borderRadius: BorderRadius.circular(8)
           ),
-          padding: const EdgeInsets.all(25),
-          child: const Text("Receipt here..."),
+           padding: const EdgeInsets.all(25),
+           child: const Text("Receipt here..."),
+            ),
+          ],
         ),
-      ],
-    ),
+      ),
     );
   }
 }
